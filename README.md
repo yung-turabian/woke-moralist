63331f6cda398db6e4db572bb85aee4ae220333e
Henry Wandover <henrywandover@gmail.com>
2024-06-10 13:04:09 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A powerful theme for NeoVim
+===
---
+++ colors/woke-moralist.lua
@@ -0,0 +1,6 @@
+vim.opt.background = 'dark'
+vim.g.colors_name = 'woke-moralist'
+package.loaded['lush_theme.woke-moralist'] = nil
+
+require('lush')(require('lush_theme.woke-moralist'))
+
